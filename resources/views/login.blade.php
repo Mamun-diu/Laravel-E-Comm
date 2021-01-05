340b56899337ca890beaa7e7d66f7e338a6de46d
Mamun <mamun511510782@gmail.com>
2021-01-05 16:29:17 +0600

    Login with Session Completed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,19 +3,21 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 offset-4">
-            <form >
+            <form action="{{ URL::to('/login') }}" method="post">
+                @csrf
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address </label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Email" aria - describedby="emailHelp">
+                    <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Enter Email" aria - describedby="emailHelp">
 
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password </label>
-                    <input type="password" class="form-control" placeholder="Password" id="exampleInputPassword1">
+                    <input type="password" class="form-control" name="password" placeholder="Password" id="exampleInputPassword1">
                 </div>
 
-                <button type="submit" class="btn btn-primary">Submit </button>
+                <button type="submit" class="btn btn-primary">Login </button>
             </form>
+            <div class=""></div>
         </div>
     </div>
 </div>
